0ae34d367b9d4b07d5a6fc54d18575e6ce644f14
phucnlvps03027@fpt.edu.vn <phucnlvps03027@fpt.edu.vn>
2016-03-02 15:39:12 +0700

    Assignment_PS03027

---
+++ assignment/Class1.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+Public Class Class1
+    Public Function Loadkhachang() As DataSet
+        Dim chuoiketnoi As String = "Data Source=.;Initial Catalog=lamchoi1;Integrated Security=True"
+        Dim conn As SqlConnection = New SqlConnection(chuoiketnoi)
+        Dim LoadKH As New SqlDataAdapter("select MAKH as 'Mã KH' ,TENKH as 'Tên Khách Hàng', DIACHI as 'Địa chỉ', SDT as 'SĐT', EMAIL from KHACHHANG1", conn)
+        Dim db As New DataSet
+        conn.Open()
+        LoadKH.Fill(db)
+        conn.Close()
+        Return db
+    End Function
+    Public Function Loadsanpham() As DataSet
+        Dim chuoiketnoi As String = "Data Source=.;Initial Catalog=lamchoi1;Integrated Security=True"
+        Dim conn As SqlConnection = New SqlConnection(chuoiketnoi)
+        Dim LoadSP As New SqlDataAdapter("select SANPHAM1.MASP as 'Mã sản phẩm',SANPHAM1.TENSP as 'Tên sản phẩm', LOAISANPHAM1.MALOAI as 'Mã Loại', LOAISANPHAM1.TENLOAI as 'Tên Loại',SANPHAM1.SOLUONG as 'Số lượng' from SANPHAM1 inner join LOAISANPHAM1 on SANPHAM1.MASP = LOAISANPHAM1.MASP", conn)
+        Dim db As New DataSet
+        conn.Open()
+        LoadSP.Fill(db)
+        conn.Close()
+        Return db
+    End Function
+End Class
---
+++ assignment/frmXemsanpham.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data.DataSet
+Public Class frmXemsanpham
+    Dim db As New DataTable
+    Dim chuoiketnoi As String = "Data Source=.;Initial Catalog=lamchoi1;Integrated Security=True"
+    Dim connect As SqlConnection = New SqlConnection(chuoiketnoi)
+    Private Sub btnXemall_Click(sender As Object, e As EventArgs) Handles btnXemall.Click
+        Dim hienthi As New Class1
+        dgvXemsp.DataSource = hienthi.Loadsanpham.Tables(0)
+    End Sub
+
+    Private Sub btnXem_Click(sender As Object, e As EventArgs) Handles btnXem.Click
+        Dim connect As SqlConnection = New SqlConnection(chuoiketnoi)
+        connect.Open()
+        Dim timkiem As SqlDataAdapter = New SqlDataAdapter("select SANPHAM1.MASP as 'Mã sản phẩm',SANPHAM1.TENSP as 'Tên sản phẩm', LOAISANPHAM1.MALOAI as 'Mã Loại', LOAISANPHAM1.TENLOAI as 'Tên Loại',SANPHAM1.SOLUONG as 'Số lượng' from SANPHAM1 inner join LOAISANPHAM1 on SANPHAM1.MASP = LOAISANPHAM1.MASP where SANPHAM1.MASP ='" & txtMASP.Text & "' ", connect)
+        Try
+            If txtMASP.Text = "" Then
+                MessageBox.Show("Bạn cần nhập mã sản phẩm", "Nhập thiếu", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                db.Clear()
+                dgvXemsp.DataSource = Nothing
+                timkiem.Fill(db)
+                If db.Rows.Count > 0 Then
+                    dgvXemsp.DataSource = db.DefaultView
+                    txtMASP.Text = Nothing
+                Else
+                    MessageBox.Show("Không tìm được")
+                    txtMASP.Text = Nothing
+                End If
+            End If
+            connect.Close()
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub btnDong_Click(sender As Object, e As EventArgs) Handles btnDong.Click
+        Me.Close()
+    End Sub
+End Class
